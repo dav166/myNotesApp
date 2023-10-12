4d616e63a8cec4cc0e8f1a78c13e0d23444642a3
David <5992264+dav166@users.noreply.github.com>
2023-10-11 21:48:01 -0500

    feat: Add header css, add imports to app component, add directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { NotesListComponent } from '../notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NotesListComponent],
 })
 export class AppComponent {}
