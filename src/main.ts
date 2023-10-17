dded411c6c9672b51b5c76a8f932266389934474
David <5992264+dav166@users.noreply.github.com>
2023-10-17 10:12:40 -0500

    feat: Import routing modules and define routing config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import 'zone.js/dist/zone';
-
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
+const routes: Routes = [
+  { path: '', component:
+  NotesListComponent },
+  { path: '', component:
+  AddNoteComponent },
+];
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
